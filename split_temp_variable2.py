308aa2811011966106e6d657fff87fbb590e66f6
Anjelica <ambrosio.anjelica@gmail.com>
2021-02-12 13:15:13 -0800

    completed exercises

--- split_temp_variable2.py
+++ split_temp_variable2.py
@@ -5,8 +5,8 @@ def save_into_db(info):
     print("saved into databse")
 
 
-user_input = input('Please enter your username: ')
-save_into_db(user_input)
-user_input = int(input('Please enter your birth year: '))
-age = 2020 - user_input
+user_username = input('Please enter your username: ')
+save_into_db(user_username)
+user_birthyear = int(input('Please enter your birth year: '))
+age = 2020 - user_birthyear
 print("You are",age, "years old.")
